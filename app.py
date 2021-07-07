ce5ae06831bf5b2636e8aafb03491ded94e3c3b5
astral8 <akulsingha8@gmail.com>
2021-07-06 21:53:24 -0400

    linked different pages

--- app.py
+++ app.py
@@ -4,5 +4,14 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def index():
+def login():
+    return render_template('login.html')
+
+
+@app.route('/home/')
+def home():
     return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

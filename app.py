3cbb6fb7cfbe156383452abfe26c17f4092c2093
astral8 <akulsingha8@gmail.com>
2021-07-06 22:49:50 -0400

    Added pages

--- app.py
+++ app.py
@@ -13,5 +13,14 @@ def home():
     return render_template('home.html')
 
 
+@app.route('/staticvideos/')
+def staticvideos():
+    return render_template('staticvideo.html')
+
+
+@app.route('/livestream')
+def livestreaming():
+    return render_template('livestream.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
